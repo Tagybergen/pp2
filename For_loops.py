e0b7169ff53788be93825ec3efb52f6a5d8b1bcf
Tagybergen <156894644+Tagybergen@users.noreply.github.com>
2024-02-01 23:16:22 +0300

    Add files via upload

---
+++ For_loops.py
@@ -0,0 +1,6 @@
+#1
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+
+#2
---
+++ OperatorExamples.py
@@ -0,0 +1,63 @@
+#1
+"""
+Operator	Name	Example	
++	Addition	x + y	
+-	Subtraction	x - y	
+*	Multiplication	x * y	
+/	Division	x / y	
+%	Modulus	x % y	
+**	Exponentiation	x ** y	
+//	Floor division	x // y
+"""
+
+#2
+"""
+Operator	Example	Same As	Try it
+=	x = 5	x = 5	
++=	x += 3	x = x + 3	
+-=	x -= 3	x = x - 3	
+*=	x *= 3	x = x * 3	
+/=	x /= 3	x = x / 3	
+%=	x %= 3	x = x % 3	
+//=	x //= 3	x = x // 3	
+**=	x **= 3	x = x ** 3	
+&=	x &= 3	x = x & 3	
+|=	x |= 3	x = x | 3	
+^=	x ^= 3	x = x ^ 3	
+>>=	x >>= 3	x = x >> 3	
+<<=	x <<= 3	x = x << 3
+
+"""
+
+#3
+"""
+
+Operator	Name	Example	Try it
+==	Equal	x == y	
+!=	Not equal	x != y	
+>	Greater than	x > y	
+<	Less than	x < y	
+>=	Greater than or equal to	x >= y	
+<=	Less than or equal to	x <= y
+
+"""
+
+#4
+"""
+Operator	Description	Example	Try it
+and 	Returns True if both statements are true	x < 5 and  x < 10	
+or	Returns True if one of the statements is true	x < 5 or x < 4	
+not	Reverse the result, returns False if the result is true	not(x < 5 and x < 10)
+
+"""
+
+#5
+"""
+
+Operator	Description	Example	Try it
+is 	Returns True if both variables are the same object	x is y	
+is not	Returns True if both variables are not the same object	x is not y
+
+"""
+
+#6
